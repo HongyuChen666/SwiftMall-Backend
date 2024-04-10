7f2b20833c025964ffb3c1da89777da230a3833c
HongyuChen666 <158306080+HongyuChen666@users.noreply.github.com>
2024-04-10 01:18:45 -0400

    Delete swiftmall-manager directory

--- swiftmall-manager/src/main/java/com/atchen/swiftmall/manager/service/ValidateCodeService.java
+++
@@ -1,7 +0,0 @@
-package com.atchen.swiftmall.manager.service;
-
-import com.atchen.swiftmall.model.vo.system.ValidateCodeVo;
-
-public interface ValidateCodeService {
-    ValidateCodeVo generateValidateCode();
-}
